66e976988af834a21c9229b21d93a05079d3159a
Glehu <duffystar1@gmail.com>
2018-02-13 15:36:47 +0100

    (NOT WORKING) Updated Project to fit current state

--- src/com/glehu/mpanne/Setup.java
+++ src/com/glehu/mpanne/Setup.java
@@ -16,14 +16,14 @@ class Setup
 		int[] shape = new int[]{784, 16, 16, 10};
 		n.setupNetwork(shape);
 
-		File[] folders = new File[10];
-		for(int dir = 0; dir < 10; dir++)
+		File[] folders = new File[1];
+		for(int dir = 0; dir < 1; dir++)
 		{
 			String dir_ = String.format("C:\\Users\\duffy\\Desktop\\training\\%d", dir);
 			folders[dir] = new File(dir_);
 			System.out.println(dir_);
 		}
 
-		n.learn(20000, 1.0f, 200,0.0f, folders);
+		n.learn(1000000, 0.3f, 200,0.0f, folders);
 	}
 } // For more information see network.java (and all the other classes too)
